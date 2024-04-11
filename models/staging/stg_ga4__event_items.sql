8d690a86565afa5119c7ad4b8c232f165eadae66
Chien Le <lchien063@gmail.com>
2024-04-11 16:02:19 +0700

    feat: ecommerce items

--- models/staging/stg_ga4__event_items.sql
+++ models/staging/stg_ga4__event_items.sql
@@ -1,9 +1,33 @@
+{{
+    config(
+        materialized = 'incremental',
+        incremental_strategy = 'insert_overwrite',
+        tags = ["incremental"],
+        partition_by={
+            "field": "event_date_dt",
+            "data_type": "date",
+            "granularity": "day"
+        },
+        on_schema_change = 'sync_all_columns',
+    )
+}}
+
+
 with items_with_params as (
     select
         event_key,
         event_name,
         event_date_dt,
         stream_id,
+        {{ ga4.unnest_key('event_params', 'transaction_id') }},
+        {{ ga4.unnest_key('event_params', 'currency') }},
+        {{ ga4.unnest_key('event_params', 'value', 'double_value') }},
+        {{ ga4.unnest_key('event_params', 'tax', 'double_value') }},
+        {{ ga4.unnest_key('event_params', 'shipping', 'double_value') }},
+        {{ ga4.unnest_key('event_params', 'affiliation', 'double_value') }},
+        {{ ga4.unnest_key('event_params', 'coupon', 'double_value') }},
+        {{ ga4.unnest_key('event_params', 'delivery_method', ) }},
+        {{ ga4.unnest_key('event_params', 'payment_type',) }},
         i.item_id,
         i.item_name,
         i.item_brand,
@@ -20,8 +44,6 @@ with items_with_params as (
         i.item_revenue,
         i.item_refund_in_usd,
         i.item_refund,
-        i.coupon,
-        i.affiliation,
         i.location_id,
         i.item_list_id,
         i.item_list_name,
@@ -33,6 +55,9 @@ with items_with_params as (
     from {{ref('stg_ga4__events')}},
         unnest(items) as i
     where event_name in ('add_payment_info', 'add_shipping_info', 'add_to_cart','add_to_wishlist','begin_checkout' ,'purchase','refund', 'remove_from_cart','select_item', 'select_promotion','view_item_list','view_promotion', 'view_item')
+    {% if is_incremental() %}
+      and event_date_dt >= date_sub(current_date, interval {{var('static_incremental_days',3) | int}} day)
+    {% endif %}
 )
 
 select * from items_with_params
